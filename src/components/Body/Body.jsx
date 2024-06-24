ed5ede5168d19c31b7419f7d4bf9c6ada78d184a
Ishanvi Chauhan <ishanvi.22b0101124@abes.ac.in>
2024-06-24 22:00:41 +0530

    Updated favicon and body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,7 +3,27 @@
 const Body= () => {
     return(
         <div>
-            
+            <div className="main">
+                <h4>Features</h4>
+                <h2>Discover Key Features</h2>
+                <p>Explore the functionalities that make Breathe Easy your go-to app for air quality monitoring and tree planting initiatives.</p>
+                <div className="features">
+                    <h3>Track Air Quality</h3>
+                    <p>Monitor real-time air quality data and pollutant concentrations to stay informed about your surroundings.</p>
+                </div>
+                <div className="features">
+                    <h3>Tree Planting Impact</h3>
+                    <p>Visualize the positive impact of planting trees on improving air quality and track the number of trees needed to reach target AQI levels.</p>
+                </div>
+                <div className="features">
+                    <h3>Suitable Tree Selection</h3>
+                    <p>Monitor real-time air quality data and pollutant concentrations to stay informed about your surroundings.</p>
+                </div>
+                <div className="features">
+                    <h3>Interactive Map</h3>
+                    <p>Monitor real-time air quality data and pollutant concentrations to stay informed about your surroundings.</p>
+                </div>
+            </div>
         </div>
     )
 }
