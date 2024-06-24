f91993c798f96d4bfb51fa837f3d4587203d77b5
KshitizSrivastav <kshitizsr02@gmail.com>
2024-06-24 23:05:51 +0530

    Update Content.jsx

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,7 +3,12 @@ import "./content.css";
 const Content= () => {
     return(
         <div>
-            Hello
+            Welcome to Breathe Easy - Your Air Quality Companion!
+            Breathe Easy is your go-to app for monitoring air quality, making a positive impact through tree planting, and expanding your
+            knowledge on environment health.With interactive features and engaging content, we empower you to control the air you breathe in.
+            By leveraging advanced algorithms and real-time data, Breathe Easy offers precise recommendations tailored to your location.
+            Whether you’re a city planner, an environmental activist, or a concerned citizen, Breathe Easy empowers you to make a tangible difference.
+            Join us in creating a healthier world, one breathe at a time.
         </div>
     )
 }
