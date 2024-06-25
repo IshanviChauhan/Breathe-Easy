7b3e7713e9c1ca9f9cf757a7fdb9841e399dcbda
Ishanvi Chauhan <ishanvi.22b0101124@abes.ac.in>
2024-06-25 23:26:16 +0530

    update features

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -5,23 +5,33 @@ const Body= () => {
         <div>
             <div className="main">
                 <h4>Features</h4>
-                <h2>Discover Key Features</h2>
+                <h1>Discover Key Features</h1>
                 <p>Explore the functionalities that make Breathe Easy your go-to app for air quality monitoring and tree planting initiatives.</p>
-                <div className="features">
-                    <h3>Track Air Quality</h3>
-                    <p>Monitor real-time air quality data and pollutant concentrations to stay informed about your surroundings.</p>
-                </div>
-                <div className="features">
-                    <h3>Tree Planting Impact</h3>
-                    <p>Visualize the positive impact of planting trees on improving air quality and track the number of trees needed to reach target AQI levels.</p>
-                </div>
-                <div className="features">
-                    <h3>Suitable Tree Selection</h3>
-                    <p>Explore a variety of tree species tailored to your local climate and soil conditions to make informed decisions for tree planting initiatives.</p>
-                </div>
-                <div className="features">
-                    <h3>Interactive Map</h3>
-                    <p>Navigate a 3D topographic map displaying color- coded AQI levels, allowing you to explore detailed air quality information for specific areas.</p>
+                <div className="features-box">
+                    <a href="#">
+                        <div className="feature">
+                            <h3><i className='bx bx-cross'></i>Track Air Quality</h3>
+                            <p>Monitor real-time air quality data and pollutant concentrations to stay informed about your surroundings.</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div className="feature">
+                            <h3><i className='bx bxs-shield-plus'></i>Tree Planting Impact</h3>
+                            <p>Visualize the positive impact of planting trees on improving air quality and track the number of trees needed to reach target AQI levels.</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div className="feature">
+                            <h3><i className='bx bxs-tree'></i>Suitable Tree Selection</h3>
+                            <p>Explore a variety of tree species tailored to your local climate and soil conditions to make informed decisions for tree planting initiatives.</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div className="feature">
+                            <h3><i className='bx bxs-map'></i>Interactive Map</h3>
+                            <p>Navigate a 3D topographic map displaying color- coded AQI levels, allowing you to explore detailed air quality information for specific areas.</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
