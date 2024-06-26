45d43a52c152608a9097bf9dcf699137ea716433
Ishanvi Chauhan <ishanvi.22b0101124@abes.ac.in>
2024-06-26 16:05:20 +0530

    update background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -59,25 +59,23 @@ const Header = () => {
         </a>
       </div>
 
-      <div id="main">
-        <div id="page1">
-          <h3>Breathe Easy</h3>
-          <p>Track, Plant and Learn for Cleaner Air</p>
+      <div id="page1">
+        <h3>Breathe Easy</h3>
+        <p>Track, Plant and Learn for Cleaner Air</p>
 
-          <div id="get">
-            <a
-              href="https://www.linkedin.com/in/zakir-uddin/"
-              target="_blank"
-              className="no-underline"
-            >
-              <div className="btn-yellow" id="btn">
-                Get Started
-              </div>
-            </a>
-            <a href="#learn-more">
-              <p>Learn More →</p>
-            </a>
-          </div>
+        <div id="get">
+          <a
+            href="https://www.linkedin.com/in/zakir-uddin/"
+            target="_blank"
+            className="no-underline"
+          >
+            <div className="btn-yellow" id="btn">
+              Get Started
+            </div>
+          </a>
+          <a href="#learn-more">
+            <p>Learn More →</p>
+          </a>
         </div>
       </div>
     </>
